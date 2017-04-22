b26f4e6419250fc89154056f6509d2da138b274b
孟祥程 <csdn@vip.163.com>
2017-04-22 11:06:57 +0800

    锁对象使用，让不同对象共用锁

--- src/com/msj/sync/MultiSyncCommonOneTest.java
+++ src/com/msj/sync/MultiSyncCommonOneTest.java
@@ -4,14 +4,17 @@ package com.msj.sync;
  * @author Vincent.M mengshaojie@188.com
  * @date 2017/3/13 下午4:05
  * @copyright ©2017 孟少杰 All Rights Reserved
- * @desc
+ * @desc 屏蔽掉对象可以测试锁对象使用，让不同对象能够同步方法
  */
 public class MultiSyncCommonOneTest {
 
     private static volatile int count = 0;
 
     public  void addCount(){
-        count ++;
+//    public  void addCount(String lock){
+//        synchronized (lock) {
+            count++;
+//        }
         System.out.println(""+count);
         try {
             Thread.sleep(1000);
@@ -22,9 +25,9 @@ public class MultiSyncCommonOneTest {
 
     public static void main(String[] args){
 
-
-
         for(int i=0;i<1000;i++){
+            //锁对象使用，让不同对象能够同步方法
+//            final String lock = "lock";
             final MultiSyncCommonOneTest test1 = new MultiSyncCommonOneTest();
             final MultiSyncCommonOneTest test2 = new MultiSyncCommonOneTest();
 
@@ -32,6 +35,7 @@ public class MultiSyncCommonOneTest {
                 @Override
                 public void run() {
                     test1.addCount();
+//                    test1.addCount(lock);
                 }
             }).start();
 
@@ -39,6 +43,7 @@ public class MultiSyncCommonOneTest {
                 @Override
                 public void run() {
                     test2.addCount();
+//                    test2.addCount(lock);
                 }
             }).start();
         }
